e80becf01482be9698e41d016a4ee6c0382d55a4
PavelTitov16 <pashoktitov16@gmail.com>
2022-02-02 02:16:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brArray = str.split('');
+  Stack = [];
+  const brConfig = {};
+  bracketsConfig.forEach(bracket => {
+    brConfig[bracket[1]] = bracket[0];
+  });
+  for (let i = 0; i < brArray.length; i++) {
+    if (Stack.length === 0) {
+      Stack.push(brArray[i]);
+    } else {
+      if (Stack[Stack.length - 1] === brConfig[brArray[i]]) {
+        Stack.pop();
+      } else {
+        Stack.push(brArray[i]);
+      }
+    }
+  }
+  return (Stack.length === 0) ? true : false;
 }
+
+
