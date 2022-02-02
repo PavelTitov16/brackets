07cf632f8bfe535d034905a4ebf0f784c722c915
PavelTitov16 <pashoktitov16@gmail.com>
2022-02-02 23:16:52 +0300

    feat: add solutin via string case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+/*module.exports = function check(str, bracketsConfig) {
+  const brArray = str.split('');
+  const stack = [];
+  const brConfig = {};
+  bracketsConfig.forEach(bracket => {
+    brConfig[bracket[1]] = bracket[0];
+  });
+  for (let i = 0; i < brArray.length; i++) {
+    if (stack.length === 0) {
+      stack.push(brArray[i]);
+    } else {
+      if (stack[stack.length - 1] === brConfig[brArray[i]]) {
+        stack.pop();
+      } else {
+        stack.push(brArray[i]);
+      }
+    }
+  }
+  return (stack.length === 0) ? true : false;
+}*/
+
 module.exports = function check(str, bracketsConfig) {
   const brArray = str.split('');
   const stack = [];
@@ -18,5 +39,3 @@ module.exports = function check(str, bracketsConfig) {
   }
   return (stack.length === 0) ? true : false;
 }
-
-
